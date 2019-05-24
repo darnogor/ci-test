fa19019bd6f5bea0dd2256f4b08a1526133e5d95
Alexander Petrenko <j.alex.ptr@gmail.com>
2019-05-24 11:00:03 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/ci_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ci_test.dir/main.cpp.obj"
+  "ci_test.pdb"
+  "ci_test.exe"
+  "ci_test.exe.manifest"
+  "libci_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ci_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello, Corehard!" << std::endl;
+    return 0;
+}
